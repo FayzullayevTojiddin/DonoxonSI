767affb1c69c4b208d98454ff4755689bb48c763
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2025-12-14 20:36:32 +0500

    Fixed Table

--- app/Filament/Resources/NotFoundData/Tables/NotFoundDataTable.php
+++ app/Filament/Resources/NotFoundData/Tables/NotFoundDataTable.php
@@ -7,7 +7,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Tables\Actions\ViewAction;
+use Filament\Actions\ViewAction;
 
 class NotFoundDataTable
 {
